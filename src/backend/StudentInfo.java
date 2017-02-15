f5dd34b5be83d80d2bc32ac5830896407e4d1156
Choffa <cgjovag@gmail.com>
2017-02-15 12:28:53 +0100

    Added some framework for the sockets

---
+++ src/backend/StudentInfo.java
@@ -0,0 +1,37 @@
+package backend;
+
+class StudentInfo{
+    byte rank;
+    byte oldRank;
+    String fag;
+
+    public StudentInfo(String fag, byte rank, byte oldRank){
+        this.fag = fag;
+        this.rank = rank;
+        this.oldRank = oldRank;
+    }
+
+    public byte getRank() {
+        return rank;
+    }
+
+    public String getFag() {
+        return fag;
+    }
+
+    public void setRank(byte rank) {
+        this.rank = rank;
+    }
+
+    public void setFag(String fag) {
+        this.fag = fag;
+    }
+
+    public byte getOldRank() {
+        return oldRank;
+    }
+
+    public void setOldRank(byte oldRank) {
+        this.oldRank = oldRank;
+    }
+}
