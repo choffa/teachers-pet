898847b12c6171f6f858717fed12f124129ad902
Choffa <cgjovag@gmail.com>
2017-02-17 13:47:53 +0100

    Read object and passed to WriterThread

--- Backend/src/backend/sockets/ConnectionHandeler.java
+++ Backend/src/backend/sockets/ConnectionHandeler.java
@@ -3,18 +3,21 @@ package backend.sockets;
 import java.io.BufferedReader;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import backend.threadsnshit.WriterThread;
 
 public class ConnectionHandeler implements Runnable {
 	
 	Socket socket;
+	WriterThread wt;
 	
-	public ConnectionHandeler(Socket socket){
+	public ConnectionHandeler(Socket socket, WriterThread wt){
 		this.socket = socket;
+		this.wt = wt;
 	}
 	@Override
 	public void run() {
 		ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
 		StudentInfo input = (StudentInfo) in.readObject();
-		//What should I do with this input?
+		wt.addInfo(input);
 	}
 }
