18ed942511bc0d2c825f035c60935e813652b767
Magnleik <magnleik@stud.ntnu.no>
2017-02-17 13:50:41 +0100

    Early interface layout for professors

---
+++ MainApplication/app/src/main/java/no/teacherspet/mainapplication/Professor.java
@@ -0,0 +1,16 @@
+package no.teacherspet.mainapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by magnus on 17.02.2017.
+ */
+
+public class Professor extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.professor_layout);
+    }
+}
