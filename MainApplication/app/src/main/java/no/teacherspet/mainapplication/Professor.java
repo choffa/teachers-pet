a622599b8802bca00b63f5d258121e467ee243cd
EirikOsnes <eirik.osnes@gmail.com>
2017-02-21 17:51:10 +0100

    Updated span of possible output numbers
    ActionBar now follows page color

--- MainApplication/app/src/main/java/no/teacherspet/mainapplication/Professor.java
+++ MainApplication/app/src/main/java/no/teacherspet/mainapplication/Professor.java
@@ -1,8 +1,10 @@
 package no.teacherspet.mainapplication;
 
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.icu.text.DecimalFormat;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +27,7 @@ public class Professor extends AppCompatActivity {
         randNumBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float avg = (float) (Math.random()*49);
+                float avg = (float) (Math.random()*40)+10;
                 avg = (float) Math.floor(avg);
                 avg = avg/10;
                 update(avg);
@@ -38,35 +40,49 @@ public class Professor extends AppCompatActivity {
         String ratingColor;
         String opacityColor;
 
-        int opacity = (int) ((average-2.5)*100); //gir et tall mellom -250 og 250 som vil gi blaa/roed hex respektivt.
+        int opacity = (int) ((average-3)*100)*250/200; //gir et tall mellom -250 og 250 som vil gi blaa/roed hex respektivt.
 
 
         opacityColor = Integer.toHexString(255-Math.abs(opacity));
-        if(opacity==0){
+
+
+        if (opacity == 0) {
             ratingColor = "#ffffff";
-        }
-        else if(opacity>255-16){
-            ratingColor = "#ff0"+ opacityColor + "0" + opacityColor; //Sterk roed
-        }
-        else if(opacity>0){
+        } else if (opacity > 255 - 16) {
+            ratingColor = "#ff0" + opacityColor + "0" + opacityColor; //Sterk roed
+        } else if (opacity > 0) {
             ratingColor = "#ff" + opacityColor + opacityColor;
-        }
-        else if(opacity<16-255){
-            ratingColor = "#0" + opacityColor+"0" + opacityColor+"ff";
-        }
-        else{
+        } else if (opacity < 16 - 255) {
+            ratingColor = "#0" + opacityColor + "0" + opacityColor + "ff";
+        } else {
             ratingColor = "#" + opacityColor + opacityColor + "ff";
         }
         return ratingColor;
 
+
+    }
+
+    public static int darker (int color, float factor) {
+        int a = Color.alpha( color );
+        int r = Color.red( color );
+        int g = Color.green( color );
+        int b = Color.blue( color );
+
+        return Color.argb( a,
+                Math.max( (int)(r * factor), 0 ),
+                Math.max( (int)(g * factor), 0 ),
+                Math.max( (int)(b * factor), 0 ) );
     }
 
     protected void update(float average){
         System.out.println(Double.toString(average));
         RelativeLayout layout;
         TextView text;
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
         layout = (RelativeLayout) findViewById(R.id.profRelLayout);
         layout.setBackgroundColor(Color.parseColor(translateColor(average)));
+        actionBar.setBackgroundDrawable(new ColorDrawable(darker(Color.parseColor(translateColor(average)), (float) 0.8)));
         text = (TextView) findViewById(R.id.treKommaFem);
         int textNum = (int) (average*10);
         text.setText(Integer.toString(textNum));
